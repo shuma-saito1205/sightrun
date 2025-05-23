5e1a4bf2f65c0db589a2c131ad9c649c4612392f
shumasaito <shushushu1205@gmail.com>
2025-05-23 04:08:58 +0000

    [Add]実装フェーズ1 5-4

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as: 'about'
   devise_for :users
   resources :posts
+  resources :users, only: [:show, :edit, :index,]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
