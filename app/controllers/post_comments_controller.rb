74bede7239388e48d87fdd2ad95509499fc961ae
shumasaito <shushushu1205@gmail.com>
2025-06-04 07:13:04 +0000

    [finish]コメント機能

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -7,7 +7,12 @@ class PostCommentsController < ApplicationController
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_id = post.id
     comment.save
-    redirect_to post_path(post_image)
+    redirect_to post_path(post)
+  end
+
+  def destroy
+    PostComment.find(params[:id]).destroy
+    redirect_to post_path(params[:post_id])
   end
 
   private
