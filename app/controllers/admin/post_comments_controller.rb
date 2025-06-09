b8853f70eaef8f4a656e33b8a35952371598dbc8
shumasaito <shushushu1205@gmail.com>
2025-06-09 07:09:26 +0000

    [finish]管理者機能

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -7,4 +7,9 @@ class Admin::PostCommentsController < ApplicationController
     @users = User.all
   end
 
+  def destroy
+    post_comment = PostComment.find(params[:id])
+    post_comment.destroy
+    redirect_to admin_post_comments_path
+  end
 end
--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -6,8 +6,15 @@ class PostCommentsController < ApplicationController
     post = Post.find(params[:post_id])
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_id = post.id
-    comment.save
-    redirect_to post_path(post)
+    if comment.save
+      redirect_to post_path(post)
+    else
+      @error_comment = comment
+      @post = Post.find(params[:post_id])
+      @user = User.find(@post.user_id)
+      @post_comment = PostComment.new
+      render template: "posts/show"
+    end
   end
 
   def destroy
