d02d12280841d2e150eb8e39ea50b5b83723cab3
shumasaito <shushushu1205@gmail.com>
2025-06-13 06:44:12 +0000

    [add]グループ　参加機能

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
     resources :post_comments, only: [:new, :create, :index, :destroy]
   end
   resources :users, only: [:show, :edit, :index, :update, :destroy]
-  resources :groups, except: [:destroy]
+  resources :groups, except: [:destroy] do
+    resource :group_users, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
