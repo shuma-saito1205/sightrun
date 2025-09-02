4c55788f043b7cb1a5f432eee1473844e267a55a
shumasaito <shushushu1205@gmail.com>
2025-09-02 09:15:25 +0000

    [add]投稿削除修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,6 +38,12 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to user_path(current_user)
+  end
+
 
   private
 
